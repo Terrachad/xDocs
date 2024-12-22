a9d98bf2dff74081819f33c776f3039adca34312
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-12-22 22:38:14 +0100

    feat: Updated the Tailwind CSS configuration file.
    Updated the borderRadius theme configuration to include more specific values for different screen sizes.
    Added a DEFAULT value to the borderRadius theme configuration to apply a default rounding value.
    Updated the keyframes theme configuration to include more specific values for the accordion-down and accordion-up animations.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,60 +10,62 @@ const config: Config = {
   ],
   prefix: '',
   theme: {
-	container: {
-		center: true,
-		padding: '2rem',
-		screens: {
-		  '2xl': '1400px',
-		  xs: '360px',
-		},
-	},
-  	extend: {
-		colors: {
-			blue: {
-			  100: '#B4C6EE',
-			  400: '#417BFF',
-			  500: '#3371FF',
-			},
-			red: {
-			  400: '#DD4F56',
-			  500: '#DC4349',
-			},
-			dark: {
-			  100: '#09111F',
-			  200: '#0B1527',
-			  300: '#0F1C34',
-			  350: '#12213B',
-			  400: '#27344D',
-			  500: '#2E3D5B',
-			},
-		  },
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-		keyframes: {
-		'accordion-down': {
-			from: { height: '0' },
-			to: { height: 'var(--radix-accordion-content-height)' },
-		},
-		'accordion-up': {
-			from: { height: 'var(--radix-accordion-content-height)' },
-			to: { height: '0' },
-		},
-		},
-		backgroundImage: {
-		doc: 'url(/assets/images/doc.png)',
-		modal: 'url(/assets/images/modal.png)',
-		},
-		animation: {
-		'accordion-down': 'accordion-down 0.2s ease-out',
-		'accordion-up': 'accordion-up 0.2s ease-out',
-		},
-		
-  	}
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+        xs: '360px',
+      },
+    },
+    extend: {
+      colors: {
+        blue: {
+          100: '#B4C6EE',
+          400: '#417BFF',
+          500: '#3371FF',
+        },
+        red: {
+          400: '#DD4F56',
+          500: '#DC4349',
+        },
+        dark: {
+          100: '#09111F',
+          200: '#0B1527',
+          300: '#0F1C34',
+          350: '#12213B',
+          400: '#27344D',
+          500: '#2E3D5B',
+        },
+      },
+      borderRadius: {
+        DEFAULT: '8px', // Apply 8px rounding by default
+        lg: '10px',     // Larger rounding
+        md: '8px',     // Medium rounding
+        sm: '6px',      // Smaller rounding
+        xl: '16px',     // Extra large rounding
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      backgroundImage: {
+        doc: 'url(/assets/images/doc.png)',
+        modal: 'url(/assets/images/modal.png)',
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
+
 export default config;
